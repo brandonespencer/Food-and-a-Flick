f04f78fd827ff2de716b2400d051d38b11cfcea7
Mike Gordon <vprmatrix55@gmail.com>
2022-01-10 20:03:31 -0700

    added movie API link to js file

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,8 @@
 // API for getting nutritional values bases off recipe ID (get from first API)
 // https://api.spoonacular.com/recipes/{id}/nutritionWidget.json
 
+// API for email function
+// https://docs.sendgrid.com/for-developers/sending-email/api-getting-started
+
+// API for movies
+// https://developers.themoviedb.org/3/getting-started/introduction
