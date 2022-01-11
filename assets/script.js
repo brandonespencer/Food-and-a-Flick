043b8b4c0d8bbd75bba8ebee460b9381e9e118d1
Mike Gordon <vprmatrix55@gmail.com>
2022-01-10 19:49:24 -0700

    added basic file structure

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// API for getting recipes by ingredients (includes image)
+// https://api.spoonacular.com/recipes/findByIngredients
+
+// API for getting nutritional values bases off recipe ID (get from first API)
+// https://api.spoonacular.com/recipes/{id}/nutritionWidget.json
+
