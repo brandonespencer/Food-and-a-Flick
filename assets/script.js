dffdb8f3b3639dcd0ed5b1f215e1b265358076b9
Mike Gordon <vprmatrix55@gmail.com>
2022-01-18 17:54:55 -0700

    resolved duplicate files

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,30 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 // Food API ID and key
 const APP_ID = "5e3b7a77";
 const APP_key = "5abc82d0bdb50534215cd56836a967fd";
